f06cea905b85370a3eed21059b4b30d542fbbd51
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-08-25 09:22:34 +0700

    "Finish Number"

--- src/main/kotlin/Number.kt
+++ src/main/kotlin/Number.kt
@@ -5,7 +5,16 @@ fun main() {
     println(age)
     println(sample)
 
-    var binary: Int = 0b0101010
+    var binary: Int = 0b010101010
     println(binary)
+
+    var price: Long = 9_000_000_000L;
+    println(price)
+
+    var priceInt: Int = price.toInt()
+    println(priceInt)
+
+    var doubleBinary: Double = binary.toDouble()
+    println(doubleBinary)
 
 }
